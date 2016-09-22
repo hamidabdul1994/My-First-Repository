7fff4c1ca6e740ccd05a599642f88781d6d720d4
Hamid Raza <hamidabdul1994@hotmail.com>
2016-09-22 19:06:01 +0530

    IPL App v 1.0.1

--- IPL Project/app.js
+++ IPL Project/app.js
@@ -1,83 +1,66 @@
 angular.module("myApp", ["firebase", "angular-carousel-3d"])
-    .controller("myIplctrl", function($scope, $firebase, $timeout) {
-
-
+    .controller("myIplctrl", function($scope, $firebase, $timeout, $log) {
 
         // SLIDES WITH CAPTIONS
-        //===================================
-        // $scope.slides = [{
-        //     'src': 'images/photo2.png',
-        //     caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, maxime.'
-        // }, {
-        //     'src': 'images/photo3.png',
-        //     caption: 'Lorem ipsum dolor sit amet '
-        // }, {
-        //     'src': 'images/photo4.png',
-        //     caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
-        // }, {
-        //     'src': 'images/photo5.png',
-        //     caption: 'Lorem ipsum dolor sit amet,  Enim, maxime.'
-        // }, {
-        //     'src': 'images/photo6.png',
-        //     caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, maxime.'
-        // }];
+        $scope.slides = [{
+            'src': 'loading.gif',
+            caption: '  '
+        }];
 
+        //OPtion Object for Setting the carousel-3d screen
         $scope.options = {
             sourceProp: 'src',
             visible: 5,
             perspective: 35,
-            startSlide: 1,
-            border: 0,
-            dir: 'ltr',
+            startSlide: 0,
+            border: 2,
+            // dir: 'ltr',
             width: 360,
-            height: 400,
-            space: 300,
-            autoRotationSpeed: 2500,
+            height: 370,
+            //space: 300,
+            clicking: true,
+            //animationSpeed:1000,
+            autoRotationSpeed: 30000,
+            //inverseScaling:1000
             loop: true
+                //controls:true
         };
 
-        var imageLoc = [],
-            imageArray = [],
-            i;
         //Refference to take data from Firebase Database
         var ref = firebase.database().ref();
         ref.on("value", function(snapshot) {
-            $timeout(imageLoc = snapshot.val().team_info, 100);
-            copyArray();
+            copyArray(snapshot.val().team_info);
         }, function(error) {
             console.log("Error: " + error.code); //
         });
 
-        var copyArray = function() {
+        //Copy the path and calling the URL path function to take Google Cloud
+        var copyArray = function(imageLoc) {
+            //var i;
+            $scope.slides = [];
             for (i in imageLoc) {
-                $scope.getMyImage(imageLoc[i].team_img_url, function(url) {
-                    imageArray.push({
+                $scope.getMyImage(imageLoc[i].team_img_url, imageLoc[i].team_name, function(url, caption) {
+                    $scope.slides.push({
                         'src': url,
-                        'caption': imageLoc[i].team_captain
+                        'caption': caption
                     });
-                    console.log(url);
                 });
             }
-            console.log("end");
-            for (i in imageArray)
-                console.log(imageArray[i].src + "----" + i + "" + imageArray[i].caption);
         }
 
-          $scope.getMyImage(path,callback){
 
-            callback(url);
-          }
-
-loh
         //Function to Get image url from google cloud
-        $scope.getMyImage = function(path, callback) {
+        $scope.getMyImage = function(path, caption, callback) {
 
             var storageRef = firebase.storage().ref();
             storageRef.child(path).getDownloadURL().then(function(url) {
 
-                callback(url);
+                callback(url, caption);
             }).catch(function(error) {
                 // Handle any errors
             });
         }
+
+
+        //Controller end
     });
